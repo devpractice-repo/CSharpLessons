11dabdc64e4087c019d92dc3be63e7070de18af6
Marat Abdrakhmanov <marat-ab@mail.ru>
2020-11-24 15:52:30 +0500

    [add] CSharpLesson6

---
+++ CSharpLesson5/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Text;
+
+namespace CSharpLesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ## Знакомство со строками в C#            
+            Console.WriteLine("## Знакомство со строками в C#");
+            string s1 = "Hello, World!";
+            Console.WriteLine(s1);
+
+            var s2 = "Create by var";
+            Console.WriteLine(s2);
+
+            string s3 = "Hello, ";
+            string s4 = s3 + "John!";
+            Console.WriteLine(s4);            
+            
+            Console.WriteLine("");
+
+            // ## Создание и инициализация объекта класса String
+            Console.WriteLine("## Создание и инициализация объекта класса String");
+            string s5 = "test1";
+            var s6 = "test2";
+
+            Console.WriteLine("first line\nSecond line");
+            Console.WriteLine("");
+            Console.WriteLine(@"first line\nSecond line");
+            Console.WriteLine("");
+
+            int age = 27;
+            Console.WriteLine(String.Format("Age: {0}", age));  // "Age: 27"
+            Console.WriteLine("");
+            Console.WriteLine($"Age: {age}"); // "Age: 27"
+            Console.WriteLine("");
+
+            string s7 = new string("test3");
+            
+            char[] charArray = {'H', 'e', 'l', 'l', 'o'};
+            string s8 = new string(charArray); // "Hello"
+
+            string s9 = new string('O', 10); // "OOOOOOOOOO"
+            Console.WriteLine("");
+
+            // ## Базовый API для работы со строками
+            // ### Объединение строк. Оператор +, методы Concat и Join
+            Console.WriteLine("## Базовый API для работы со строками");
+            Console.WriteLine("### Объединение строк. Оператор +, методы Concat и Join");
+            string s10 = "Area";
+            string s11 = " 51";
+            Console.WriteLine("Concate by +: " + s10 + s11);
+
+            Console.WriteLine("Concate by Concate(): " + string.Concat(s10, s11));
+
+            string[] sArr1 = {"First ", "Second ", "Third "};
+            Console.WriteLine("Join elements in array by Concate(): " + string.Concat(sArr1));
+
+            string[] sArr2 = {"First", "Second", "Third"};
+            Console.WriteLine("Join elements in array by Join() with space: " + string.Join(" ", sArr2));
+            Console.WriteLine("Join elements in array by Join() with <->: " + string.Join("<->", sArr2));
+            Console.WriteLine("");
+
+            // ### Поиск и извлечение элементов из строки. 
+            // Оператор [], методы IndexOf, IndexOfAny, LastIndexOf, LastIndexOfAny, Substring
+            Console.Write("### Поиск и извлечение элементов из строки. ");
+            Console.Write("Оператор [], методы IndexOf, IndexOfAny, LastIndexOf, LastIndexOfAny, Substring");
+            string s12 = "Hello";
+            Console.WriteLine("Get element by index s12[3]: " + s12[3]);
+
+            // s1 = "Hello, World!"
+            // Поиск первого вхождения символа 'r'
+            Console.WriteLine("Index of \'r\': " + s1.IndexOf('r')); 
+            // Поиск первого вхождения символа 'l' начиная с позиции 4
+            Console.WriteLine("Index of \'l\', start at 4: " + s1.IndexOf('l', 4));
+            // Поиск первого вхождения строки "World"
+            Console.WriteLine("Index of \"World\": " + s1.IndexOf("World")); 
+            // Поиск первого вхождения символа из набора ['o', 'd', ',']
+            Console.WriteLine("Index of pos of any symbol in array: " + s1.IndexOfAny(new char[] {'o', 'd', ','})); 
+
+            // Поиск последнего вхождения символа 'l'
+            Console.WriteLine("Last index of \'l\': " + s1.LastIndexOf('l')); 
+            // Поиск последнего вхождения строки "or"
+            Console.WriteLine("Last index of \"or\": " + s1.LastIndexOf("or")); 
+            // Поиск последнего вхождения символа из набора ['o', 'd', ',']
+            Console.WriteLine("Last index of pos of any symbol in array: " + s1.LastIndexOfAny(new char[] {'o', 'd', ','})); 
+
+            // Извлечение подстроки
+            Console.WriteLine("Substring start at pos 7: " + s1.Substring(7));  // World!
+            Console.WriteLine("Substring start at pos 7 (4 chars): " + s1.Substring(7, 4)); // Worl
+
+            // Проверка наличия подстроки
+            Console.WriteLine("Contains \"World\"? " + s1.Contains("World")); // True
+            Console.WriteLine("Starts with \"He\"? " + s1.StartsWith("He")); // True
+            Console.WriteLine("Ends with \"ld\"? " + s1.EndsWith("ld")); // False
+            Console.WriteLine("");
+
+            // ### Сравнение срок
+            Console.WriteLine("### Сравнение срок");
+            string t1 = "John";
+            string t2 = "John";
+            string t3 = "Mary";
+            Console.WriteLine("t1 == t2: " + (t1 == t2));   // True
+            Console.WriteLine("t1 != t2: " + (t1 != t2));   // False
+            Console.WriteLine("t1 == t3: " + (t1 == t3));   // False
+
+            Console.WriteLine("Equals method: t1.Equals(t2)" + t1.Equals(t2));   // True
+            Console.WriteLine("Equals method: t1.Equals(t3)" + t1.Equals(t3));   // False
+            Console.WriteLine("");
+
+            // ### Модификация срок ----
+            Console.WriteLine("### Модификация срок");
+            // Вставка, удаление и замена
+            Console.WriteLine("Insert: " + "123789".Insert(3, "[456]")); // 123[456]789
+            Console.WriteLine("PadLeft: ");
+            Console.WriteLine("some string".PadLeft(15)); // "    some string"
+            Console.WriteLine("some string".PadLeft(15, '*')); // "****some string"
+            Console.WriteLine("PadRight: ");
+            Console.WriteLine("some string".PadRight(15)); // "some string    "
+            Console.WriteLine("some string".PadRight(15, '*')); // "some string****"
+            Console.WriteLine("Remove demo1: " + "Hello".Remove(2));
+            Console.WriteLine("Remove demo2: " + "Hello".Remove(2, 2));
+            Console.WriteLine("Hello, World!".Replace('!', '.')); // Hello, World.
+            Console.WriteLine("Hello, World!".Replace("World", "John")); // Hello, John!
+            // Управление регистром
+            Console.WriteLine("Hello, World!".ToUpper()); // HELLO, WORLD!
+            Console.WriteLine("Hello, World!".ToLower()); // hello, world!
+            // Удаление начальных и конечных символов строки
+            Console.WriteLine("   hello   ".Trim());    // "hello"
+            Console.WriteLine("***hello---".Trim('*'));    // "hello---"
+            Console.WriteLine("***hello---".Trim(new char[] {'*', '-'}));    // "hello"
+            Console.WriteLine("   hello   ".TrimStart());   // "hello   "            
+            Console.WriteLine("   hello   ".TrimEnd()); // "   hello"
+            Console.WriteLine("");
+
+            //### Методы и свойства общего назначения
+            Console.WriteLine("### Методы и свойства общего назначения");
+            Console.WriteLine("Hello".Length); // 5
+            foreach(var s in "1 2 3".Split(' '))
+                Console.WriteLine(s);
+
+            foreach(var s in "1 2 3-4-5-6".Split(new char[]{' ', '-'}))
+                Console.WriteLine(s);
+            
+            Console.WriteLine("");
+
+            //## Форматирование строк
+            Console.WriteLine("## Форматирование строк");
+            // ToString
+            Console.WriteLine(12345.ToString("X"));
+            
+            // String.Format
+            Console.WriteLine(string.Format("value: {0}", 1.23456));
+            Console.WriteLine(string.Format("value: {0:F}", 1.23456));
+            Console.WriteLine(string.Format("value: {0:d}", DateTime.Now));
+            
+            // WriteLine без использования String.Format
+            Console.WriteLine("value: {0}", 1.23456);   // 1,23456
+            Console.WriteLine("value: {0:F}", 1.23456); // 1.235
+            Console.WriteLine("date: {0:d}", DateTime.Now); // 07.09.2020
+
+            Console.WriteLine("Only index: {0}", 123); // Only index: 123
+            Console.WriteLine("Index with aligment: {0,-5}{1,5}", 123, 456); // Index with aligment: 123    456
+            Console.WriteLine("Index with format: 0x{0:X}", 123); // Index with format: 0x7B
+            Console.WriteLine("");
+
+            // ### Представление чисел
+            Console.WriteLine("### Представление чисел");
+            Console.WriteLine("C symbol: {0:C}", 123);      // 123,00 ₽
+            Console.WriteLine("D symbol: {0:D5}", 123);     // 00123
+            Console.WriteLine("E symbol: {0:E}", 123456789);// 1,234568E+008
+            Console.WriteLine("F symbol: {0:F2}", 123.4567);// 123,46
+            Console.WriteLine("P symbol: {0:P}", 0.123);    // 123,46
+            Console.WriteLine("X symbol: 0x{0:X}", 567);    // 0x237
+            Console.WriteLine("0 symbol: {0:000.00}", 12.6789);// 012,68
+            Console.WriteLine("# symbol: {0:##}", 14.6789); // 15
+            Console.WriteLine("");
+
+            // ### Представление даты и времени
+            Console.WriteLine("### Представление даты и времени");
+            Console.WriteLine("d symbol: {0:d}", DateTime.Now);
+            Console.WriteLine("D symbol: {0:D}", DateTime.Now);
+            Console.WriteLine("f symbol: {0:f}", DateTime.Now);
+            Console.WriteLine("F symbol: {0:F}", DateTime.Now);
+            Console.WriteLine("g symbol: {0:g}", DateTime.Now);
+            Console.WriteLine("G symbol: {0:G}", DateTime.Now);
+            Console.WriteLine("t symbol: {0:t}", DateTime.Now);
+            Console.WriteLine("T symbol: {0:T}", DateTime.Now);
+            Console.WriteLine("{0:yyyy-MM-dd}", DateTime.Now);
+            Console.WriteLine("{0:dd/MM/yy}", DateTime.Now);
+            Console.WriteLine("{0:dd/MM/yy HH:mm:ss}", DateTime.Now);
+            Console.WriteLine("");
+
+            //## $ — интерполяция строк
+            Console.WriteLine("## $ — интерполяция строк");
+            int n1 = 45678;
+            double d1 = 123.34567;
+            bool b1 = true;
+            string sv = "test";
+            Console.WriteLine($"int val: {n1}, double val: {d1:#.###}");
+            Console.WriteLine($"bool val: {b1}, string val: {sv}");
+            Console.WriteLine("");
+
+            // ## Управляющие символы (литералы)
+            Console.WriteLine("## Управляющие символы (литералы)");
+            Console.WriteLine("\aName:\t\"John\"\nAge:\t\"27\"");
+
+            // ## @ - буквальный идентификатор
+            Console.WriteLine("## @ - буквальный идентификатор");
+            Console.WriteLine(@"escape is not work: \a\t\n\x1234");
+            Console.WriteLine("");
+
+            // ## Класс StringBuilder
+            Console.WriteLine("## Класс StringBuilder");
+            string outString = "";
+            for(int i = 0; i < 10; i++)
+            {
+                outString += i.ToString() + " - ";
+            }
+            Console.WriteLine(outString);
+
+            var sb = new StringBuilder();
+            for(int i = 0; i < 10; i++)
+            {
+                sb.Append(i.ToString());
+                sb.Append(" - ");
+            }
+            outString = sb.ToString();
+            Console.WriteLine(outString);
+        }
+    }
+}
---
+++ CSharpLesson6/Program.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharpLesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ## Условные операторы
+            // ### Оператор if
+            Console.WriteLine("## Условные операторы");
+            Console.WriteLine("### Оператор if");
+
+            int n1 = 9;
+            int n2 = 12;
+            if (n1 < n2)
+            {
+                Console.WriteLine($"Число {n1} меньше числа {n2}");
+            }
+
+            int n3 = 15;
+            int n4 = 12;
+            if (n3 < n4)
+            {
+                Console.WriteLine($"Число {n3} меньше числа {n4}");
+            }
+            else
+            {
+                Console.WriteLine($"Число {n3} больше числа {n4}");
+            }
+
+            int n5 = 13;
+            int n6 = 12;
+            if (n5 < n6)
+            {
+                Console.WriteLine($"Число {n5} меньше числа {n6}");
+            }
+            else if (n5 > n6)
+            {
+                Console.WriteLine($"Число {n5} больше числа {n6}");
+            }
+            else
+            {
+                Console.WriteLine($"Число {n5} равно числу {n6}");
+            }
+
+            // ### Оператор switch            
+            Console.WriteLine("### Оператор switch");
+            int n7 = 1;
+            switch (n7)
+            {
+                case 1: Console.WriteLine("Case 1"); break;
+                case 2: Console.WriteLine("Case 2"); break;
+                default: Console.WriteLine("Default case"); break;
+            }
+
+            // #### Шаблон константы          
+            Console.WriteLine("#### Шаблон константы");
+            string s1 = "Three";
+            switch (s1.ToLower())
+            {
+                case "one": Console.WriteLine("Case one"); break;
+                case "two": Console.WriteLine("Case two"); break;
+                case "three": Console.WriteLine("Case three"); break;
+                case "four": Console.WriteLine("Case four"); break;
+                case "five": Console.WriteLine("Case five"); break;
+                default: Console.WriteLine("Default case"); break;
+            }
+
+            // #### Шаблон типа          
+            Console.WriteLine("#### Шаблон типа");
+            object trans = new Moto() { Name = "Suzuki" };
+            switch (trans)
+            {
+                case Bicycle bicycle: Console.WriteLine($"Bicycle: {bicycle.Name}"); break;
+                case Moto moto: Console.WriteLine($"Moto: {moto.Name}"); break;
+                case Car car: Console.WriteLine($"Car {car.Name}"); break;
+                case Transport transport: Console.WriteLine($"Transport {transport.Name}"); break;
+                case null: Console.WriteLine("Transport is null!"); break;
+            }
+
+            // #### Выражение с ключевым словом when        
+            Console.WriteLine("#### Выражение с ключевым словом when");
+            object bc = new Bicycle() { Name = "Trec", WheelsCount = 1 };
+            switch (bc)
+            {
+                case Bicycle bicycle when bicycle.WheelsCount == 1: Console.WriteLine($"Bicycle: {bicycle.Name}, type - monocycle"); break;
+                case Bicycle bicycle when bicycle.WheelsCount == 2: Console.WriteLine($"Bicycle: {bicycle.Name}, type - classic"); break;
+                case Bicycle bicycle when bicycle.WheelsCount == 3: Console.WriteLine($"Bicycle: {bicycle.Name}, type - tricycle"); break;
+                case Moto moto: Console.WriteLine($"Moto: {moto.Name}"); break;
+                case Car car: Console.WriteLine($"Car {car.Name}"); break;
+                case Transport transport: Console.WriteLine($"Transport {transport.Name}"); break;
+                case null: Console.WriteLine("Transport is null!"); break;
+            }
+
+            // ### Тернарный оператор     
+            Console.WriteLine("### Тернарный оператор");
+            int n8 = 5;
+            int n9 = 8;
+            int largerNumber = n8 > n9 ? n8 : n9;
+
+            // ## Циклы
+            // ### Цикл for
+            Console.WriteLine("## Циклы");
+            Console.WriteLine("### Цикл for");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Квадрат числа {i} = {i * i}");
+            }
+
+            // ### Циклы while и do/while
+            Console.WriteLine("### Циклы while и do/while");
+            Console.WriteLine("-> Цикл while");
+            int index1 = 0;
+            int n10 = 3;
+            while (index1 <= n10)
+            {
+                Console.WriteLine($"Итерация цикла номер {index1}");
+                index1++;
+            }
+
+            Console.WriteLine("-> Цикл do while");
+            int index2 = 0;
+            int n11 = 3;
+            do
+            {
+                Console.WriteLine($"Итерация цикла номер {index2}");
+                index2++;
+            } while (index2 > n11);
+
+            // ### Цикл foreach
+            Console.WriteLine("### Цикл foreach");
+            int[] nums = { 6, 3, 6, 8, 9, 12, 4, 5, 88, 54, 3, 66, 78, 10, 12, 5, 7, 9, 3, 5 };
+            int result = 0;
+            foreach (int n in nums)
+            {
+                if (n > 10)
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine($"Количество чисел в массиве больше 10: {result}");
+
+            result = 0;
+            foreach (var n in nums)
+            {
+                if (n < 10)
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine($"Количество чисел в массиве меньше 10: {result}");
+
+            // ### Операторы перехода
+            Console.WriteLine("### Операторы перехода");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 10)
+                {
+                    Console.WriteLine($"{nums[i]} > 10");
+                    continue;
+                }
+                if (i > 7)
+                {
+                    Console.WriteLine("Break cycle");
+                    break;
+                }
+            }
+
+            // ### LINQ как инструмент обхода коллекций
+            Console.WriteLine("### LINQ как инструмент обхода коллекций");
+
+            var evenSq = new List<int>();
+            foreach (var n in nums)
+            {
+                if (n % 2 == 0)
+                {
+                    evenSq.Add(n * n);
+                }
+            }
+            foreach (var n in evenSq)
+            {
+                Console.Write($"{n} ");
+            }
+            Console.WriteLine();
+
+            evenSq = nums
+                        .Where(v => v % 2 == 0)
+                        .Select(v => v * v)
+                        .ToList();
+
+            foreach (var n in evenSq)
+            {
+                Console.Write($"{n} ");
+            }
+            Console.WriteLine();
+
+            var arr1 = nums.Where(v => v > 10).ToList(); // Take elements thats higher then 10
+            var arr2 = nums.Select(v => v - 7).ToList(); // Sub 7 from every elements
+            var arr3 = nums.Select(v => v % 2 == 0).ToList();
+
+            // Football example
+            var teams = CreateList();
+            var selectedTeams = from team in teams          // определяем каждый элемент из teams как team
+                                where team.PlaceGroup == 1  // фильтрация по критерию PlaceGroup == 1
+                                orderby team.Name           // упорядочиваем по имени
+                                select team;                // выбираем объект
+
+            foreach (var team in selectedTeams)
+            {
+                Console.WriteLine(team.Name + " " + team.NumberpointsScored);
+            }
+
+        }
+        private static List<FootballTeam> CreateList()
+        {
+            return new List<FootballTeam>
+            {
+                { new FootballTeam() { Name="Zenit", Country="Russia", Group= 1, PlaceGroup= 3, NumberpointsScored = 3}},
+                { new FootballTeam() { Name="Ajax", Country="Holand", Group= 1, PlaceGroup= 2, NumberpointsScored = 4}},
+                { new FootballTeam() { Name="Manchester United", Country="England", Group= 1, PlaceGroup= 1, NumberpointsScored = 6}},
+                { new FootballTeam() { Name="Bavaria", Country="Germany", Group= 2, PlaceGroup= 1, NumberpointsScored = 8}},
+                { new FootballTeam() { Name="Spartak", Country="Russia", Group= 2, PlaceGroup= 2, NumberpointsScored= 6}},
+                { new FootballTeam() { Name="Real", Country="Italy", Group= 2, PlaceGroup= 3, NumberpointsScored = 3}},
+                { new FootballTeam() { Name="Arsenal", Country="England", Group= 3, PlaceGroup= 2, NumberpointsScored = 9}},
+                { new FootballTeam() { Name="Shakter", Country="Ukrane", Group=3, PlaceGroup= 3, NumberpointsScored = 6}},
+                { new FootballTeam() { Name="Barselona", Country="Espane", Group= 3, PlaceGroup= 1, NumberpointsScored = 12}}
+            };
+        }
+
+    }
+    public class FootballTeam
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Group { get; set; }
+        public int PlaceGroup { get; set; }
+        public int NumberpointsScored { get; set; }
+    }
+
+
+    class Transport
+    {
+        public string Name { get; set; }
+        public int WheelsCount { get; set; }
+    }
+    class Bicycle : Transport { }
+    class Moto : Transport { }
+    class Car : Transport { }
+}
